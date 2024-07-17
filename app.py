5ebf8eaa5fc1a91f24f5e485bd2bacecf21271dc
IZUMIE <101053941+IZUMIE-MIYAMURA@users.noreply.github.com>
2024-07-17 11:30:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,5 +59,29 @@ def random_cum_image():
         print(f"Error occurred: {e}")
         return jsonify(error=str(e)), 500
 
+@app.route('/logo', methods=['GET'])
+def random_neko_image():
+    try:
+        # Ensure the JSON file exists
+        if not os.path.isfile(LOGO_IMAGE):
+            return jsonify(error="JSON file does not exist"), 404
+
+        # Read the JSON file
+        with open(NEKO_IMAGE, 'r') as file:
+            image_urls = json.load(file)
+
+        # Ensure the JSON file is not empty
+        if not image_urls:
+            return jsonify(error="No images found in JSON file"), 404
+
+        # Select a random image URL
+        random_image_url = random.choice(image_urls)
+
+        # Return the JSON response with the image URL
+        return jsonify(url=random_image_url), 200
+    except Exception as e:
+        # Log the error and return a 500 response
+        print(f"Error occurred: {e}")
+        return jsonify(error=str(e)), 500
 if __name__ == '__main__':
     app.run(debug=True)
